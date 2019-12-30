dd604922a264e2ee4a366f05d122ae2a977cb5dc
Uttam Kumar Manna <45291428+ukml2018@users.noreply.github.com>
2019-12-30 20:04:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://$MYSQL_USER:$MYSQL_PASSWORD@$HOSTNAME/$MYSQL_DATABASE'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://xxuser:welcome1@127.0.0.1/sampledb'
-app.secret_key = "flask rocks!"
+#app.secret_key = "flask rocks!"
 
 db = SQLAlchemy(app)
